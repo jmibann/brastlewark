86d3652f1c028650b6d38822dbca56ee149465bf
Juan Manuel <juanmanuel@juani.local>
2021-11-08 15:07:56 -0300

    Minor style fix

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -4,7 +4,7 @@ const Spinner: React.FC<{}> = () => {
   return (
     <div className="flex flex-col justify-around items-center sm:flex-row w-full sm:h-12 mb-4">
       <div
-        className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-purple-500 my-14 sm-my-0"
+        className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-purple-500 py-0.5 my-12 sm-my-0 sm:py-0 sm:my-0"
       />
     </div>
   )
